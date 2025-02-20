5461835d24a8b133b338ff684e4a75ef3a9a6d4e
WonsoHero <junyho123@naver.com>
2025-02-20 11:25:14 +0900

    이동 주석 해제

--- Assets/LeeByeoungJoon/Yut/YutResults.cs
+++ Assets/LeeByeoungJoon/Yut/YutResults.cs
@@ -19,28 +19,28 @@ public class YutResults : NetworkBehaviour
         switch (yutResult)
         {
             case YutResult.BackDo:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToPrevNode(1);
-                Debug.Log("-1");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToPrevNode(1);
+                //Debug.Log("-1");
                 break;
             case YutResult.Do:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(1);
-                Debug.Log("1");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(1);
+                //Debug.Log("1");
                 break;
             case YutResult.Gae:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(2);
-                Debug.Log("2");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(2);
+                //Debug.Log("2");
                 break;
             case YutResult.Gur:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(3);
-                Debug.Log("3");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(3);
+                //Debug.Log("3");
                 break;
             case YutResult.Yut:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(4);
-                Debug.Log("4");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(4);
+                //Debug.Log("4");
                 break;
             case YutResult.Mo:
-                //GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(5);
-                Debug.Log("5");
+                GameManager.Instance.mainGameProgress.currentCharacter.MoveToNextNode(5);
+                //Debug.Log("5");
                 break;
         }
         //결과 리스트에서 뺌
