89af582dc80a90cf093fdc047bec2f5a8ef711ff
sch990428 <990428sch@naver.com>
2025-02-25 17:55:52 +0900

    클래스 구조 및 주석 정리
    클래스 구조 및 주석 정리

---
+++ Assets/Scripts/MinigameManager/Define.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Define
+{
+    public enum MinigameType
+    {
+        StackGame, // 스택게임
+        ShootingGame, // 사격게임
+        RunningGame, // 달리기게임
+    }
+}
--- Assets/Scripts/MinigameManager/MinigameManager.cs
+++ Assets/Scripts/MinigameManager/MinigameManager.cs
@@ -1,37 +1,41 @@
 using System.Collections.Generic;
-using System.Resources;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MinigameManager : MonoBehaviour
 {
-    public enum MinigameType
+    private Dictionary<Define.MinigameType, string> MinigameScenes = new Dictionary<Define.MinigameType, string>()
     {
-        StackGame, // 스택게임
-        ShootingGame, // 사격게임
-        RunningGame, // 달리기게임
-        //CatchingGame, // 바구니게임
-    }
-
-    public Dictionary<MinigameType, string> MinigameScenes = new Dictionary<MinigameType, string>()
-    {
-        { MinigameType.StackGame, "StackScene" },
-        { MinigameType.ShootingGame, "ShootingScene" },
-        { MinigameType.RunningGame, "RunGame" },
-        //{ MinigameType.CatchingGame, "BasketGame" }
+        { Define.MinigameType.StackGame, "StackScene" },
+        { Define.MinigameType.ShootingGame, "ShootingScene" },
+        { Define.MinigameType.RunningGame, "RunGame" },
     };
 
     // 미니게임을 시작하기 위해 씬을 이동
-    public void StartMinigame(MinigameType type)
+    public void StartMinigame(Define.MinigameType type)
     {
-        string sceneName = MinigameScenes[type];
-        NetworkManager.Singleton.SceneManager.LoadScene(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        if (NetworkManager.Singleton.IsServer)
+        {
+            if (MinigameScenes.TryGetValue(type, out string sceneName))
+            {
+                NetworkManager.Singleton.SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+            }
+            else
+            {
+                Debug.LogError($"씬 {type}이 없음");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("서버가 아니라 씬 변경 못함");
+        }
     }
 
     // 씬에서 버튼에 직접 연결하기 위해 임시로 만든 메서드
-    // 추후 코드상에서는 위의 enum 매개변수 타입을 사용
+    // 추후 코드상에서는 위의 enum 매개변수 타입을 사용할 것
     public void StartMinigame(int i)
     {
-        StartMinigame((MinigameType)i);
+        StartMinigame((Define.MinigameType)i);
     }
 }
