12bc32b5697b3a94fea51c0ebd973b03478b5c94
WonsoHero <junyho123@naver.com>
2025-03-07 17:52:26 +0900

    네트워크 변수 동기화 문제
    - 읽기 권한 모두에게 허용

--- Assets/Scripts/SM_Hwang/MainGame/Character/CharacterInfo.cs
+++ Assets/Scripts/SM_Hwang/MainGame/Character/CharacterInfo.cs
@@ -2,7 +2,7 @@ using Unity.Netcode;
 
 public class CharacterInfo : NetworkBehaviour
 {
-    public NetworkVariable<bool> inIsland = new NetworkVariable<bool>(false);
-    public NetworkVariable<bool> canMove = new NetworkVariable<bool>(true);
+    public NetworkVariable<bool> inIsland = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone);
+    public NetworkVariable<bool> canMove = new NetworkVariable<bool>(true, NetworkVariableReadPermission.Everyone);
     public int overlappedCount = 0;
 }
